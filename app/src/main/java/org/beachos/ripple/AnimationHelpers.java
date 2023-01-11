c3f591c2f4fba4a55e8ed3bde2cf70589f34420a
root <Beach OS>
2023-01-11 05:38:17 +0000

    BeachOS

---
+++ app/src/main/java/org/beachos/ripple/AnimationHelpers.java
@@ -0,0 +1,50 @@
+package org.beachos.ripple;
+
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+
+public class AnimationHelpers {
+
+    public static void translateY(final View view, float fromY, float toY, long duration) {
+        if (duration == 0)
+            view.setTranslationY(toY);
+        else
+            view.animate().translationY(toY).setDuration(duration).start();
+    }
+
+    public static void scale(final View view, float fromScale, float toScale, long duration, final Runnable whenDone) {
+        if (duration == 0) {
+            view.setScaleX(toScale);
+            view.setScaleY(toScale);
+            if (whenDone != null)
+                whenDone.run();
+        } else {
+            ViewPropertyAnimator animation = view.animate().scaleX(toScale).scaleY(toScale).setDuration(duration);
+            if (whenDone != null) {
+                animation.setListener(new AnimatorListener() {
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        whenDone.run();
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        whenDone.run();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+                    }
+
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                    }
+
+                });
+            }
+            animation.start();
+        }
+    }
+}
---
+++ app/src/main/java/org/beachos/ripple/PanicTileService.java
@@ -0,0 +1,21 @@
+package org.beachos.ripple;
+
+import android.content.Intent;
+import android.os.Build;
+import android.service.quicksettings.TileService;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class PanicTileService extends TileService {
+    public static final String TAG = "PanicTileService";
+
+    @Override
+    public void onClick() {
+        super.onClick();
+
+        Intent intent = new Intent(this, CountDownActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityAndCollapse(intent);
+    }
+}
